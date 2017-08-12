e6fb7a3da5c902cec04263146c7e7d0fbd6159e1
Quang Hoang <quanghoang@Quangs-MacBook-Pro.local>
2017-08-12 22:05:52 +0200

    init

--- hackerrank langugage.py
+++
@@ -1,4 +0,0 @@
-import re
-pattern = re.compile(r'^(BASH|BRAINFUCK|C(LISP|LOJURE|PP|SHARP)?|D(ART)?|ERLANG|GO|GROOVY|HASKELL|JAVA(SCRIPT)?|LUA|OBJECTIVEC|OCAML|PASCAL|PERL|PHP|PYTHON|R(UBY)?|SBCL|SCALA)$')
-for _ in range(int(input())):
-    print('VALID' if re.match(pattern, raw_input().split()[1]) else 'INVALID')
